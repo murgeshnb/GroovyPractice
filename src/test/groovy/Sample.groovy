65ea50bd67e7b8f8028137ee2d96a6c6bbbe1ae9
murgeshnb <mnbechetti@gmail.com>
2022-06-19 23:07:09 +0530

    hey only 2 for creating ... .gitignore file

---
+++ src/test/groovy/Sample.groovy
@@ -0,0 +1,10 @@
+class Sample {
+    static void main(args){
+        println("Welcome to groovy");
+
+        def employeeNAme="Murgesh";
+        def r=5;
+        print(employeeNAme+" "+r)
+
+    }
+}
